0f61598a59ad47300824a76c5bd25b0c86c684b6
zhangfengming <zhangfengming@baidu.com>
2017-01-01 17:53:03 +0800

    Change: get_complete_position algorithm

--- rplugin/python3/deoplete/sources/deoplete_padawan.py
+++ rplugin/python3/deoplete/sources/deoplete_padawan.py
@@ -12,11 +12,13 @@ from urllib.error import URLError
 from socket import timeout
 import sys
 import re
+import logging as logger
 
 sys.path.insert(1, path.dirname(__file__) + '/deoplete_padawan')
 
 import padawan_server  # noqa
 
+logger.basicConfig(level=logger.INFO)
 
 class Source(Base):
 
@@ -45,35 +47,47 @@ class Source(Base):
         self.server = padawan_server.Server(server_addr, server_command,
                                             log_file)
 
+    def get_cursor_column(self, context):
+        if self.current.window.cursor is tuple:
+            return self.current.window.cursor[1]
+        return len(context['input'])
+
     def get_complete_position(self, context):
-        patterns = [r'[\'"][^\)]*$', r'[\w\\]*$']
+        cursor_col = self.get_cursor_column(context)
         input = context['input']
+        if cursor_col < len(input):
+            logger.debug('using cursor column')
+            # not at the end of line
+            return cursor_col
+        patterns = [
+            r'(?:.*)::$|::(?=[_a-zA-Z][_\w]*$)', # static method/prop
+            r'(?:.*)->$|->(?=[_a-zA-Z][_\w]*$)', # instance method/prop
+            r'(?:.*)\\$|\\(?=[_a-zA-Z][_\w]*$)', # namespace
+            r'(?:.*)\$$|\$(?=[_a-zA-Z][_\w]*$)', # variable
+            r'(?:.*)[_\w]+\s*?\((?=.*?)',      # method/function call
+        ]
         pos = self.get_patterns_position(context, patterns)
-        if pos in range(len(input)) and input[pos] == '\\':
-            pos = pos + 1
+        if pos < 0:
+            logger.debug('No match')
+            return len(input)
+        if pos in range(len(input)):
+            if input[pos] == '\\':
+                pos += 1
+            elif input[pos] == '(':
+                pos -= 1
         return pos
 
     def get_padawan_column(self, context):
-        patterns = [r'(?<=[\s\(\=])\w+$|^\w+$']
-        pos = self.get_patterns_position(context, patterns)
-        if pos == context['complete_position']:
-            return pos + 2
-        return context['complete_position'] + 1
+        return self.get_complete_position(context) + 1
 
     def get_patterns_position(self, context, patterns):
         result = -1
-        result_end = -1
-        pos = -1
+        logger.debug('Matching in %s', context['input'])
         for pattern in patterns:
+            logger.debug('Using pattern %s', pattern)
             m = re.search(pattern, context['input'])
-            if m:
-                pos = m.start()
-                pos_end = m.end()
-            # match new pattern only if previous one ended before this one
-            if pos > result_end:
-                result = pos
-                result_end = pos_end
-
+            if m and m.end() > result:
+                result = m.end()
         return result
 
     def gather_candidates(self, context):
@@ -118,7 +132,7 @@ class Source(Base):
 
     def get_candidate_word(self, item):
         signature = self.get_candidate_signature(item)
-        name = self.get_candidate_abbr(item)
+        name = item['name']
         if self.add_parentheses != 1:
             return name
         if signature.find('()') == 0:
